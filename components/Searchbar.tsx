720bf4622f52edbc02fd7a2ec10fb1b10793bf13
pandawaa <pallakangoding@gmail.com>
2024-01-18 20:59:27 +0800

    Scrape Amazon Product

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,7 +1,47 @@
 "use client"
 
+import { scrapeAndStoreProduct } from "@/lib/actions"
+import { FormEvent, useState } from "react"
+
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parseURL = new URL(url)
+    const hostname = parseURL.hostname
+
+    if (
+      hostname.includes(
+        "amazon.com" ||
+          hostname.includes("amazon.") ||
+          hostname.endsWith("amazon")
+      )
+    ) {
+      return true
+    }
+  } catch (error) {
+    return false
+  }
+  return false
+}
+
 const Searchbar = () => {
-  const handleSubmit = () => {}
+  const [searchPrompt, setSearchPrompt] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const isValidLink = isValidAmazonProductURL(searchPrompt)
+
+    if (!isValidLink) return alert("Please provide a valid Amazone link")
+
+    try {
+      setIsLoading(true)
+      const product = await scrapeAndStoreProduct(searchPrompt)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
@@ -9,9 +49,15 @@ const Searchbar = () => {
         type="text"
         placeholder="Enter product link"
         className="searchbar-input"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
       />
-      <button type="submit" className="searchbar-btn">
-        Search
+      <button
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchPrompt === ""}
+      >
+        {isLoading ? "Searching..." : "Search"}
       </button>
     </form>
   )
---
+++ lib/actions/index.ts
@@ -0,0 +1,14 @@
+"use server"
+
+import { scrapeAmazonProduct } from "../scraper"
+
+export async function scrapeAndStoreProduct(productUrl: string) {
+  if (!productUrl) return
+
+  try {
+    const scrapeProduct = await scrapeAmazonProduct(productUrl)
+    if (!scrapeProduct) return
+  } catch (error: any) {
+    throw new Error(`Failed to create/update product: ${error.message}`)
+  }
+}
